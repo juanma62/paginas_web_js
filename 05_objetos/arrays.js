53c67cd438840fc5f9c83765329131b24b48bc6d
juanma62 <jsalazarmanuel@yahoo.es>
2018-07-12 11:06:33 +0200

    Cambios_12-7-18_descanso

--- 05_objetos/arrays.js
+++ 05_objetos/arrays.js
@@ -3,3 +3,11 @@ let oDatos = {}
 
 let aDatos = [1,2]
 // aDatos = new Array()
+
+Math.PI
+
+
+console.log(parseInt(Math.random()*10))
+console.log(parseInt(Math.random()*10))
+console.log(parseInt(Math.random()*10))
+console.log(parseInt(Math.random()*10))
---
+++ 05_objetos/dni.js
@@ -0,0 +1,9 @@
+
+
+function calcularLetra(num){
+    const LETRAS = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+    return LETRAS[num%23]
+    
+}
+
+console.log(calcularLetra(5384484))
