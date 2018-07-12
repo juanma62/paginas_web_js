2a613b6daad2cc7571a64b219809cca313b80b18
juanma62 <jsalazarmanuel@yahoo.es>
2018-07-12 10:01:52 +0200

    Cambios_libros

---
+++ 05_objetos/arrays.js
@@ -0,0 +1,5 @@
+let oDatos = {}
+// oDatos = new Object()
+
+let aDatos = [1,2]
+// aDatos = new Array()
---
+++ 05_objetos/elementales.js
@@ -0,0 +1,26 @@
+let x = 23
+let oX = new Number(23)
+
+console.log(typeof x)
+console.log(typeof oX)
+console.log(oX * 2)
+
+'22' * 2
+// Number('22') * 2
+
+let name = 'Pepe'
+let aDatos = []
+
+
+function numbers() {
+    let num = 23
+    console.log(num)
+    console.log(num.toFixed(2))
+    let numPrecision = num.toFixed(2)
+    console.log(typeof numPrecision)
+    console.log(typeof num)
+    console.log(numPrecision * 3)
+
+}
+
+numbers()
