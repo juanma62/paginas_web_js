315f879c8cee942360d505f72e9eb70e185a2cb1
juanma62 <jsalazarmanuel@yahoo.es>
2018-07-31 12:18:22 +0200

    Cambios_31-7-18_descanso

--- 13_web_eventos/scroll/app.js
+++ 13_web_eventos/scroll/app.js
@@ -1,25 +1,52 @@
-class App{
-    constructor(){
+class App {
 
-        this.btnTop = document.querySelector('#btnTop')
-        this.divTop = document.querySelector('#divTop')
+    constructor () {
+        this.btnTop = document.querySelector('#btn-top')
+        this.divTop = document.querySelector('#div-top')
+        this.header = document.querySelector('header')
+        this.parrafoHeader = document.querySelector('header p')
+        this.bajoHeader = document.querySelector('#bajo-header')
 
-        document.addEventListener('scroll', this.scrollDetect.bind(this))
+        document.addEventListener('scroll', 
+            this.scollDetect.bind(this) )
         this.btnTop.addEventListener('click', 
             this.subir.bind(this))
-    }
-
-    scrollDetect(oE){
-        if(oE.target.scrollingElement.scrollTop > 95){
-            this.divTop.classList.remove('oculto')
+    }   
+    
+    scollDetect (oE) {
+        if (oE.target.scrollingElement.scrollTop > 20) {
+            // sticky header 
+            this.header.classList.add('sticky-header')
+            this.header.classList.remove('normal-header')
+            this.parrafoHeader.classList.add('hide')
+            this.bajoHeader.classList.add('bajo-header')
+            this.bajoHeader.classList.remove('hide')
         } else {
-            this.divTop.classList.add('oculto')
+            // normal header
+            this.header.classList.remove('sticky-header')
+            this.header.classList.add('normal-header')
+            this.parrafoHeader.classList.remove('hide')
+            this.bajoHeader.classList.remove('bajo-header')
+            this.bajoHeader.classList.add('hide')
+        }
+        
+        if (oE.target.scrollingElement.scrollTop > 100) {
+            this.divTop.classList.remove('hide')
+        } else {
+            this.divTop.classList.add('hide')
         }
     }
+
     subir(oE) {
-        console.log('Subiendo');
-        document.scrollingElement.scrollTop = 0
+        //document.scrollingElement.scrollTop = 0
+        //location.assign()
+        //location.href = "#"
+        window.scroll({
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth'
+        })
     }
 }
 
-document.addEventListener("DOMContentLoaded", ()=> {new  App()})
+document.addEventListener('DOMContentLoaded', ()=> {new  App()})
